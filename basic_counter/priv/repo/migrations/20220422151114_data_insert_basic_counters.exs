9ed339e38ec46a7944cc00d7cec95e0420f0fb8e
Ilia Borovitinov <ilia@vaxine.io>
2022-05-03 16:16:11 +0300

    Updated the migration so the conflict doesn't trigger migration failure

--- basic_counter/priv/repo/migrations/20220422151114_data_insert_basic_counters.exs
+++ basic_counter/priv/repo/migrations/20220422151114_data_insert_basic_counters.exs
@@ -3,9 +3,9 @@ defmodule Counters.Repo.Migrations.DataInsertBasicCounters do
   alias Counters.Reactions.Reaction
 
   def up do
-    repo().insert(%Reaction{id: "multi-user", count: 0})
-    repo().insert(%Reaction{id: "multiplayer", count: 0})
-    repo().insert(%Reaction{id: "geo-distributed", count: 0})
-    repo().insert(%Reaction{id: "other", count: 0})
+    repo().insert(%Reaction{id: "multi-user", count: 0}, on_conflict: :nothing)
+    repo().insert(%Reaction{id: "multiplayer", count: 0}, on_conflict: :nothing)
+    repo().insert(%Reaction{id: "geo-distributed", count: 0}, on_conflict: :nothing)
+    repo().insert(%Reaction{id: "other", count: 0}, on_conflict: :nothing)
   end
 end
