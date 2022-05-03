dbea7c84095e1c33d1dcaa5c372ffed66091cdd3
Ilia Borovitinov <ilia@vaxine.io>
2022-05-03 19:48:45 +0300

    Added pipeline triggering for deploy (#14)
    Triggers a special pipeline in vaxine-io/mesh repository

---
+++ .buildkite/trigger_deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Set up a variable to hold the meta-data from your block step
+DEMO_NAME="$(buildkite-agent meta-data get image_tag)"
+DEMO_IMAGE_TAG="$(buildkite-agent meta-data get demo_name)"
+
+# Create a pipeline with your trigger step
+PIPELINE="steps:
+  - trigger: \"deploy-demos\"
+    label: \"Trigger deploy\"
+    build:
+      meta_data:
+        demo-name: $DEMO_NAME
+        demo-image-tag: $DEMO_IMAGE_TAG
+"
+
+# Upload the new pipeline and add it to the current build
+echo "$PIPELINE" | buildkite-agent pipeline upload
