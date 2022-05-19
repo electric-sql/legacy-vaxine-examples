3badf474604d216d589d88d77ce25e46bb96a533
Ilia Borovitinov <ilia@vaxine.io>
2022-05-19 12:31:09 +0300

    Added an option to request data from multiple relays

--- advanced_counter/apps/advanced_counter_web/lib/advanced_counter_web/relay_integration.ex
+++ advanced_counter/apps/advanced_counter_web/lib/advanced_counter_web/relay_integration.ex
@@ -1,24 +1,40 @@
 defmodule AdvancedCounterWeb.RelayIntegration do
   @database_list ["cloudsql", "cockroach"]
 
-  def increment_counter(databases \\ @database_list, id) when is_binary(id) do
-    Enum.map(databases, &increment_one_counter(&1, id))
+  @spec stream_counter_increment(list(String.t()), list(String.t()), String.t()) :: Enumerable.t()
+  def stream_counter_increment(servers, databases \\ @database_list, id) do
+    pairs = for x <- servers, y <- databases, do: {x, y}
+
+    Task.async_stream(pairs, fn {server, db} -> increment_one_counter(server, db, id) end,
+      max_concurrency: length(pairs),
+      ordered: false,
+      on_timeout: :kill_task
+    )
+    |> Stream.zip(pairs)
+    |> Stream.map(fn
+      {{:ok, response}, _} -> response
+      {{:exit, reason}, {server, db}} -> {server, db, {:error, "Network error: #{reason}"}}
+    end)
   end
 
-  defp increment_one_counter(database, id) when database in @database_list do
-    Finch.build(:post, "http://localhost:4001/api/increment/#{database}/#{id}")
+  def increment_counter(server, databases \\ @database_list, id) when is_binary(id) do
+    Enum.map(databases, &increment_one_counter(server, &1, id))
+  end
+
+  defp increment_one_counter(server, database, id) when database in @database_list do
+    Finch.build(:post, "#{server}/api/increment/#{database}/#{id}")
     |> Finch.request(Relay)
     |> case do
       {:ok, %Finch.Response{body: body, status: 200}} ->
         %{"write_latency" => latency} = Jason.decode!(body)
-        {database, {:ok, latency}}
+        {server, database, {:ok, latency}}
 
       {:ok, %Finch.Response{body: body, status: 503}} ->
         %{"error" => error} = Jason.decode!(body)
-        {database, {:error, error}}
+        {server, database, {:error, error}}
 
       {:error, %Mint.TransportError{reason: reason}} ->
-        {database, {:error, "Network error: #{reason}"}}
+        {server, database, {:error, "Network error: #{reason}"}}
     end
   end
 end
